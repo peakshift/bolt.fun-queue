5c8f12ab68f139a600ddb6b96ffae3fe054109e8
David <davidisadev@gmail.com>
2023-08-08 12:06:25 -0500

    feat (meilisearch): added meilisearch add doc and remove doc logic

--- src/workers/relay_worker.ts
+++ src/workers/relay_worker.ts
@@ -16,5 +16,22 @@ export const createRelayMeilisearchWorker = (queueName = 'relay-meilisearch') =>
 
             const relayPool = createRelaysPool();
 
+            try {
+                let index = job.data.type 
+                let data = job.data.payload
+
+                switch (job.data.action) {
+                    case 'create' || 'update':
+                        await client.index(index).addDocuments(data)
+                        break;
+                    case 'delete':
+                        await client.index(index).deleteDocument(data.id)
+                }
+            } catch (error) {
+                console.log(error);
+                throw error;
+            } finally {
+                relayPool.close();
+            }
         }
     );
